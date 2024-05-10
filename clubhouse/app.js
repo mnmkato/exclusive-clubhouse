eb411cd6161973580e751b4962c2edf085f9375d
mnmkato <katsmamal@gmail.com>
2024-05-10 15:42:04 +0000

    set up basic authentication

--- clubhouse/app.js
+++ clubhouse/app.js
@@ -7,17 +7,70 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
+const User = require('./models/user')
+
+/* DB connection*/
+const mongoDb = "mongodb+srv://katsmamal:dbkatsmamal@cluster0.9yy3ykh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" })
+      }
+     
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    };
+  })
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- clubhouse/routes/index.js
+++ clubhouse/routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
+const passport = require("passport");
+const bcrypt = require("bcryptjs")
+const User = require('../models/user')
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", (req, res) => {
+  res.render("index", { 
+    title: "Clubhouse" ,
+    user: req.user
+  });
+}); 
+
+/* GET sign up page. */
+router.get("/sign-up", (req, res) => res.render("sign-up", {title: "clubhouse" }));
+
+/* POST sign up page form */
+router.post("/sign-up", async (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    // if err, do something
+    if(err){consoole.log(err)}
+
+    // otherwise, store hashedPassword in DB
+    try {
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword
+      });
+      const result = await user.save();
+      res.redirect("/");
+    } catch(err) {
+      return next(err);
+    };
+  });
+  });
+
+/* POST log in page form */
+router.post(
+    "/log-in",
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/"
+    })
+  );
+
+/* GET log out */
+router.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
 
 module.exports = router;
